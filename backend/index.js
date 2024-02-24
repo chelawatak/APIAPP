bbd58638619b5032978741766e5cc2a083d36885
chelawatak <chelawatak@gmail.com>
2024-02-25 02:10:51 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT||3000;
 
+const allowedOrigins = [
+    "https://aniket-note-storing.netlify.app/",
+];
+
+
 app.use(cors());
 app.use(express.json());
 
